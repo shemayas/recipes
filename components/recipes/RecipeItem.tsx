f039585de162a5a9ef9f02c003dd28298d84309d
unknown <speroim@gmail.com>
2024-06-16 00:36:59 +0300

    הוספת אפקט קו מתקדם לטקסט אחרי התמונה

--- components/recipes/RecipeItem.tsx
+++ components/recipes/RecipeItem.tsx
@@ -1,23 +1,27 @@
 import Image from "next/image";
 import { FC } from "react";
+import "../../styles/styles.css"; // ודאו שהנתיב לקובץ ה-CSS מתאים
 
 const RecipeItem: FC<Recipe> = ({ categoryIds, text, title, imagePath }) => {
   return (
-    <div className="my-4">
-      <h2 className="text-2xl font-bold mb-2">{title}</h2>
+    <div className="my-4 hover:cursor-pointer group relative">
+      <h2 className="text-2xl font-bold mb-2 relative">{title}</h2>
       {imagePath && (
-        <Image
-          src={imagePath}
-          alt={title}
-          className="w-full h-64 object-cover mb-4"
-          width={500}
-          height={500}
-        />
+        <div className="w-full aspect-w-16 aspect-h-9 mb-4 relative">
+          <Image 
+            src={imagePath}
+            alt={title}
+            layout="fill"
+            className="rounded-lg transition-transform duration-1000 transform group-hover:scale-105 object-cover"
+          />
+        </div>
       )}
-      <p>{text}</p>
+      <p className="transition-opacity duration-500 group-hover:opacity-100 relative line-animation">
+                    
+        {text}
+      </p>
     </div>
   );
 };
-
 
 export default RecipeItem;
