711e046e59a7e7d95541d61caed18f0bb48b5ec7
Shemaya Spero <shemayas@gmail.com>
2024-08-29 14:45:12 +0300

    Add markdown support, move to use encode

--- app/recipe/[slug]/page.tsx
+++ app/recipe/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import data from "@/data/data.json";
+import { marked } from "marked";
 
 const { recipes } = data;
 interface RecipeSlugProps {
@@ -10,7 +11,9 @@ const RecipeSlug: FC<RecipeSlugProps> = ({ params: { slug } }) => {
   console.log(slug);
   const recipe = recipes.find((recipe) => recipe.slug === decodeURI(slug));
   console.log({ recipe });
-  return <div>{JSON.stringify(recipe)}</div>;
+  const decode = decodeURI(recipe?.text || "");
+  const html = marked(decode);
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
 };
 
 export default RecipeSlug;
